94997f55f10e15884a6d2061da978d300da56454
Angelo de Mello <Angelo.deMello@iam.local>
2019-02-04 15:23:33 -0600

    Questionaire

---
+++ OOP/QuestionnaireAssignment/QuestionAssingment/QuestionAssingment/Form1.Designer.vb
@@ -0,0 +1,286 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.CheckedListBoxQuestion1 = New System.Windows.Forms.CheckedListBox()
+        Me.CheckedListBoxQuestion2 = New System.Windows.Forms.CheckedListBox()
+        Me.CheckedListBoxQuestion3 = New System.Windows.Forms.CheckedListBox()
+        Me.CheckedListBoxQuestion4 = New System.Windows.Forms.CheckedListBox()
+        Me.CheckedListBoxQuestion5 = New System.Windows.Forms.CheckedListBox()
+        Me.CheckedListBoxQuestion6 = New System.Windows.Forms.CheckedListBox()
+        Me.CheckedListBoxQuestion7 = New System.Windows.Forms.CheckedListBox()
+        Me.CheckedListBoxQuestion8 = New System.Windows.Forms.CheckedListBox()
+        Me.CheckedListBoxQuestion9 = New System.Windows.Forms.CheckedListBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(47, 66)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(115, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Am the life of the party."
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(230, 66)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(140, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Feel little concern for others."
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(428, 66)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(105, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Am always prepared."
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(609, 66)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(116, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Get stressed out easily."
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(811, 66)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(120, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Have a rich vocabulary."
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(47, 244)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(78, 13)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Don't talk a lot."
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(230, 244)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(120, 13)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "Am interested in people."
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(428, 244)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(146, 13)
+        Me.Label8.TabIndex = 7
+        Me.Label8.Text = "Leave my belongings around." & Global.Microsoft.VisualBasic.ChrW(9)
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(609, 244)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(139, 13)
+        Me.Label9.TabIndex = 8
+        Me.Label9.Text = "Am relaxed most of the time."
+        '
+        'CheckedListBoxQuestion1
+        '
+        Me.CheckedListBoxQuestion1.CheckOnClick = True
+        Me.CheckedListBoxQuestion1.FormattingEnabled = True
+        Me.CheckedListBoxQuestion1.Items.AddRange(New Object() {"Very Inaccurate", "Moderately Inaccurate", "Nor Inaccurate", "Moderately Accurate", "Very Accurate"})
+        Me.CheckedListBoxQuestion1.Location = New System.Drawing.Point(50, 103)
+        Me.CheckedListBoxQuestion1.Name = "CheckedListBoxQuestion1"
+        Me.CheckedListBoxQuestion1.Size = New System.Drawing.Size(138, 79)
+        Me.CheckedListBoxQuestion1.TabIndex = 9
+        '
+        'CheckedListBoxQuestion2
+        '
+        Me.CheckedListBoxQuestion2.CheckOnClick = True
+        Me.CheckedListBoxQuestion2.FormattingEnabled = True
+        Me.CheckedListBoxQuestion2.Items.AddRange(New Object() {"Very Inaccurate", "Moderately Inaccurate", "Nor Inaccurate", "Moderately Accurate", "Very Accurate"})
+        Me.CheckedListBoxQuestion2.Location = New System.Drawing.Point(233, 103)
+        Me.CheckedListBoxQuestion2.Name = "CheckedListBoxQuestion2"
+        Me.CheckedListBoxQuestion2.Size = New System.Drawing.Size(138, 79)
+        Me.CheckedListBoxQuestion2.TabIndex = 10
+        '
+        'CheckedListBoxQuestion3
+        '
+        Me.CheckedListBoxQuestion3.CheckOnClick = True
+        Me.CheckedListBoxQuestion3.FormattingEnabled = True
+        Me.CheckedListBoxQuestion3.Items.AddRange(New Object() {"Very Inaccurate", "Moderately Inaccurate", "Nor Inaccurate", "Moderately Accurate", "Very Accurate"})
+        Me.CheckedListBoxQuestion3.Location = New System.Drawing.Point(431, 103)
+        Me.CheckedListBoxQuestion3.Name = "CheckedListBoxQuestion3"
+        Me.CheckedListBoxQuestion3.Size = New System.Drawing.Size(138, 79)
+        Me.CheckedListBoxQuestion3.TabIndex = 11
+        '
+        'CheckedListBoxQuestion4
+        '
+        Me.CheckedListBoxQuestion4.CheckOnClick = True
+        Me.CheckedListBoxQuestion4.FormattingEnabled = True
+        Me.CheckedListBoxQuestion4.Items.AddRange(New Object() {"Very Inaccurate", "Moderately Inaccurate", "Nor Inaccurate", "Moderately Accurate", "Very Accurate"})
+        Me.CheckedListBoxQuestion4.Location = New System.Drawing.Point(612, 103)
+        Me.CheckedListBoxQuestion4.Name = "CheckedListBoxQuestion4"
+        Me.CheckedListBoxQuestion4.Size = New System.Drawing.Size(138, 79)
+        Me.CheckedListBoxQuestion4.TabIndex = 12
+        '
+        'CheckedListBoxQuestion5
+        '
+        Me.CheckedListBoxQuestion5.CheckOnClick = True
+        Me.CheckedListBoxQuestion5.FormattingEnabled = True
+        Me.CheckedListBoxQuestion5.Items.AddRange(New Object() {"Very Inaccurate", "Moderately Inaccurate", "Nor Inaccurate", "Moderately Accurate", "Very Accurate"})
+        Me.CheckedListBoxQuestion5.Location = New System.Drawing.Point(814, 103)
+        Me.CheckedListBoxQuestion5.Name = "CheckedListBoxQuestion5"
+        Me.CheckedListBoxQuestion5.Size = New System.Drawing.Size(138, 79)
+        Me.CheckedListBoxQuestion5.TabIndex = 13
+        '
+        'CheckedListBoxQuestion6
+        '
+        Me.CheckedListBoxQuestion6.CheckOnClick = True
+        Me.CheckedListBoxQuestion6.FormattingEnabled = True
+        Me.CheckedListBoxQuestion6.Items.AddRange(New Object() {"Very Inaccurate", "Moderately Inaccurate", "Nor Inaccurate", "Moderately Accurate", "Very Accurate"})
+        Me.CheckedListBoxQuestion6.Location = New System.Drawing.Point(50, 295)
+        Me.CheckedListBoxQuestion6.Name = "CheckedListBoxQuestion6"
+        Me.CheckedListBoxQuestion6.Size = New System.Drawing.Size(138, 79)
+        Me.CheckedListBoxQuestion6.TabIndex = 14
+        '
+        'CheckedListBoxQuestion7
+        '
+        Me.CheckedListBoxQuestion7.CheckOnClick = True
+        Me.CheckedListBoxQuestion7.FormattingEnabled = True
+        Me.CheckedListBoxQuestion7.Items.AddRange(New Object() {"Very Inaccurate", "Moderately Inaccurate", "Nor Inaccurate", "Moderately Accurate", "Very Accurate"})
+        Me.CheckedListBoxQuestion7.Location = New System.Drawing.Point(233, 295)
+        Me.CheckedListBoxQuestion7.Name = "CheckedListBoxQuestion7"
+        Me.CheckedListBoxQuestion7.Size = New System.Drawing.Size(138, 79)
+        Me.CheckedListBoxQuestion7.TabIndex = 15
+        '
+        'CheckedListBoxQuestion8
+        '
+        Me.CheckedListBoxQuestion8.CheckOnClick = True
+        Me.CheckedListBoxQuestion8.FormattingEnabled = True
+        Me.CheckedListBoxQuestion8.Items.AddRange(New Object() {"Very Inaccurate", "Moderately Inaccurate", "Nor Inaccurate", "Moderately Accurate", "Very Accurate"})
+        Me.CheckedListBoxQuestion8.Location = New System.Drawing.Point(431, 295)
+        Me.CheckedListBoxQuestion8.Name = "CheckedListBoxQuestion8"
+        Me.CheckedListBoxQuestion8.Size = New System.Drawing.Size(138, 79)
+        Me.CheckedListBoxQuestion8.TabIndex = 16
+        '
+        'CheckedListBoxQuestion9
+        '
+        Me.CheckedListBoxQuestion9.CheckOnClick = True
+        Me.CheckedListBoxQuestion9.FormattingEnabled = True
+        Me.CheckedListBoxQuestion9.Items.AddRange(New Object() {"Very Inaccurate", "Moderately Inaccurate", "Nor Inaccurate", "Moderately Accurate", "Very Accurate"})
+        Me.CheckedListBoxQuestion9.Location = New System.Drawing.Point(612, 295)
+        Me.CheckedListBoxQuestion9.Name = "CheckedListBoxQuestion9"
+        Me.CheckedListBoxQuestion9.Size = New System.Drawing.Size(138, 79)
+        Me.CheckedListBoxQuestion9.TabIndex = 17
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(47, 491)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(37, 13)
+        Me.Label10.TabIndex = 18
+        Me.Label10.Text = "Result"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(90, 488)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 19
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1046, 617)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.CheckedListBoxQuestion9)
+        Me.Controls.Add(Me.CheckedListBoxQuestion8)
+        Me.Controls.Add(Me.CheckedListBoxQuestion7)
+        Me.Controls.Add(Me.CheckedListBoxQuestion6)
+        Me.Controls.Add(Me.CheckedListBoxQuestion5)
+        Me.Controls.Add(Me.CheckedListBoxQuestion4)
+        Me.Controls.Add(Me.CheckedListBoxQuestion3)
+        Me.Controls.Add(Me.CheckedListBoxQuestion2)
+        Me.Controls.Add(Me.CheckedListBoxQuestion1)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents CheckedListBoxQuestion1 As CheckedListBox
+    Friend WithEvents CheckedListBoxQuestion2 As CheckedListBox
+    Friend WithEvents CheckedListBoxQuestion3 As CheckedListBox
+    Friend WithEvents CheckedListBoxQuestion4 As CheckedListBox
+    Friend WithEvents CheckedListBoxQuestion5 As CheckedListBox
+    Friend WithEvents CheckedListBoxQuestion6 As CheckedListBox
+    Friend WithEvents CheckedListBoxQuestion7 As CheckedListBox
+    Friend WithEvents CheckedListBoxQuestion8 As CheckedListBox
+    Friend WithEvents CheckedListBoxQuestion9 As CheckedListBox
+    Friend WithEvents Label10 As Label
+    Friend WithEvents TextBox1 As TextBox
+End Class
