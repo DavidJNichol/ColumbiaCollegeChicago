9ada4b2d49285709bb66f1c0af4c2b4ac692c822
unknown <angelocasagrande@live.com>
2019-04-02 12:58:58 -0500

    GaiProjectGame Unity

---
+++ GAI/GaiProjectGame/Assets/Scripts/BehaviorAI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BehaviorAI : MonoBehaviour
+{
+    public GameObject target = null;
+    private NavMeshAgent nma = null;
+
+    // Use this for initialization
+    void Start()
+    {
+        nma = this.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        nma.SetDestination(target.transform.position);
+    }
+}
